8fa891d2d44308aa63966b7be031a6b85c5755ef
RefatAli <mohamedrefat8434@gmail.com>
2023-07-18 13:44:20 +0300

    push gallery

--- js/singleSlider.js
+++ js/singleSlider.js
@@ -1,6 +1,27 @@
 // slider single
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide(".splide", {
+    type: "loop",
+    perPage: 3, // Number of visible slides
+    gap: "30px", // Gap between slides
+    focus: "center",
+
+    pagination: true,
+
+    classes: {
+      pagination: "splide__pagination your-class-pagination",
+      page: "splide__pagination__page your-class-page",
+    },
+  }).mount();
+});
+
 var swiper = new Swiper(".gallerSlider", {
-  effect: "cards",
-  centeredSlides: true,
-  grabCursor: true,
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
